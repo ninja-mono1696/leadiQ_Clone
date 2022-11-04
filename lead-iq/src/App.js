8d5a5b26e6b194a70791d96945589d62375abf42
Monojit Mondal <mondalmonojit1696@gmail.com>
2022-11-05 04:03:19 +0530

    code update

--- lead-iq/src/App.js
+++ lead-iq/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import AllRoutes from './components/AllRoutes';
+import AllRoutes from './pages/AllRoutes';
 import Navbar from './components/Navbar/navbar';
 import SectionDark from './components/Section/Dark_Section/section-dark';
 import SectionNeutral from './components/Section/SectionPart2/lightSection';
 import WfSection from './components/Section/SectionPart3/wfSection';
 import SectionBlueLight from './components/Section/SectionPart4/SectionBlueLight';
+import Footer from './components/Footer/Footer';
 
 function App() {
 return (
@@ -15,6 +16,7 @@ return (
 		<SectionNeutral />
 		<WfSection />
 		<SectionBlueLight />
+		<Footer />
 	</>
 );
 }
